117383515a935d409859dea80e24b9cf242d115c
ktotah <ket2137@columbia.edu>
2024-06-12 22:47:29 -0400

    Updated Mongoose version and removed deprecated options, corrected route handler in thoughtRoutes.js

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB');
 
 module.exports = mongoose.connection;
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -14,7 +14,7 @@ const {
 router.route("/").get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 router.route("/:thoughtId/reactions").post(addReaction);
