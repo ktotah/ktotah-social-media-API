705effcdcf11ef584d9a2a7b432bfe61c1748b7a
ktotah <ket2137@columbia.edu>
2024-06-12 23:58:22 -0400

    modified connection.js to add a console log message to confirm the connection to the MongoDB database

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 module.exports = mongoose.connection;
